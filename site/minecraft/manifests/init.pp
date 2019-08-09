b97676ab807844eb5a9246e60c151fe90d750af9
cnguyen1994 <brian.nguyen1994@gmail.com>
2019-08-09 15:03:26 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,9 +2,14 @@ class minecraft {
   file {'/opt/minecraft':
     ensure => directory,
   }
+  exec {'retrieve_server':
+    command => '/usr/bin/wget -q https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar -O /opt/minecraft/server.jar',
+    creates => '/opt/minecraft/server.jar',
+  }
   file {'/opt/minecraft/server.jar':
     ensure => file,
-    source      => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar',
+    mode   => 0755,
+    source => Exec["retrieve_server"],
   }
   package {'java':
     ensure => present,
