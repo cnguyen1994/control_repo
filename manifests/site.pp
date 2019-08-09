a2feca70cc950c0bc2e4adaa65af004f792876f3
cnguyen1994 <brian.nguyen1994@gmail.com>
2019-08-09 12:54:56 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,14 +8,14 @@ node 'master.puppet.vm' {
   }
 }
 
+node 'minetest.puppet.vm' {
+  include role::minecraft_server
+}
+
 node /^web/ {
   include role::app_server
 }
 
 node /^db/ {
   include role::db_server
-}
-
-node 'minetest.puppet.vm' {
-  include role::minecraft_server
 }
