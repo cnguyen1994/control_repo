2dc686325bffa668680882861b021e5cc0fcaee3
cnguyen1994 <brian.nguyen1994@gmail.com>
2019-08-09 12:30:40 -0400

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,20 @@
+class minecratf {
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+  file {'/opt/minecraft/server.jar':
+    ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar',
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'/opt/minecraft/eula.txt':
+    ensure  => file,
+    context => 'eula=true',
+  }
+  file {'/ect/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+}
