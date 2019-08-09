e8a8ae5f9f2cb28157676c48010279044950b8a3
cnguyen1994 <brian.nguyen1994@gmail.com>
2019-08-09 15:22:13 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,18 +1,10 @@
 class minecraft {
-  include vcsrepo
   file {'/opt/minecraft':
     ensure => directory,
   }
-  vcsrepo { '/opt/minecraft/server.jar':
-    ensure   => present,
-    provider => wget,
-    source   => 'https://github.com/technomancy/leiningen.git',
-    revision => 'stable',
-  }
   file {'/opt/minecraft/server.jar':
     ensure => file,
-    mode   => '0755',
-    source => Exec["retrieve_server"],
+    source => "https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar"
   }
   package {'java':
     ensure => present,
