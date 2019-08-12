274cc40f18b6c2ec054268b0812298c46f224d3f
cnguyen1994 <brian.nguyen1994@gmail.com>
2019-08-12 16:32:13 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,14 +1,20 @@
 class minecraft {
+  include wget
   file {'/etc/environment':
     ensure => file,
     content => 'http_proxy=http://www-proxy-hqdc.us.oracle.com:80; https_proxy=http://http://www-proxy-hqdc.us.oracle.com:80; proxy=http://www-proxy-hqdc.us.oracle.com:80'
+  }
+  wget::retrieve { "download Minecraft server":
+    source      => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar',
+    destination => '/opt/minecraft/server.jar',
+    timeout     => 0,
+    verbose     => false,
   }
   file {'/opt/minecraft':
     ensure => directory,
   }
   file {'/opt/minecraft/server.jar':
     ensure => file,
-    source => "https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar"
   }
   package {'java':
     ensure => present,
