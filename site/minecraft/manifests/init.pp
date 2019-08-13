63dc2d43baaba19b601ccad3106cd2fb9af6b59c
cnguyen1994 <brian.nguyen1994@gmail.com>
2019-08-13 15:07:44 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,18 +1,22 @@
 class minecraft {
-  file {'/opt/minecraft':
+  $url = 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar'
+  $proxy_url = 'http://www-proxy-hqdc.us.oracle.com:80'
+  $install_dir = '/opt/minecraft'
+  file {$install_dir:
     ensure => directory,
   }
   exec { 'Set Proxy': 
-    command => 'echo "export http_proxy=http://www-proxy-hqdc.us.oracle.com:80 && export https_proxy=http://www-proxy-hqdc.us.oracle.com:80 && export proxy=http://www-proxy-hqdc.us.oracle.com:80" >> ~/.bashrc && source ~/.bashrc && curl https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar -o /opt/minecraft/server.jar', 
+    command => 'echo "export http_proxy=${proxy_url} && export https_proxy=${proxy_url} && export proxy=${proxy_url}" >> ~/.bashrc && source ~/.bashrc && curl ${url} -o ${install_dir}/server.jar', 
     path    => ['/usr/bin', '/usr/sbin',],
    }
-  file {'/opt/minecraft/server.jar':
+  file {"${install_dir}/server.jar":
     ensure => file,
+    before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
   }
-  file {'/opt/minecraft/eula.txt':
+  file {"${install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true',
   }
@@ -23,5 +27,6 @@ class minecraft {
   service { 'minecraft':
     ensure => running,
     enable => true,
+    required => [Package['java'],File['${install_dir}/eula.txt'],File['/etc/systemd/system/minecraft.service']],
   }
 }
