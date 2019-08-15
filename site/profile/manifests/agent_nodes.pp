403da32ab0aaf709771dc0083415ab4dfb1dc263
cnguyen1994 <brian.nguyen1994@gmail.com>
2019-08-15 11:10:51 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,18 +1,28 @@
 class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
+  dockeragent::node {'web1.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
+  dockeragent::node {'db1.puppet.vm':}
   dockeragent::node {'minetest.puppet.vm':}
   host {'web.puppet.vm': 
     ensure => present,
     ip     => '172.18.0.2',
   }
-  host {'db.puppet.vm':
+  host {'web1.puppet.vm': 
     ensure => present,
     ip     => '172.18.0.3',
   }
-  host {'minetest.puppet.vm':
+  host {'db.puppet.vm':
     ensure => present,
     ip     => '172.18.0.4',
+  }
+  host {'db.puppet.vm':
+    ensure => present,
+    ip     => '172.18.0.5',
+  }
+  host {'minetest.puppet.vm':
+    ensure => present,
+    ip     => '172.18.0.6',
   }
 }
